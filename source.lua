e2696e499c4d820c8f9461ac79ff1ae1437ead4d
Alex <117677548+OfficialAlexCoder@users.noreply.github.com>
2024-10-22 18:19:59 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,2 +1,2 @@
 wait("0.2")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OfficialAlexCoder/killbrickhalloweenthing/refs/heads/main/source.lua", true()))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OfficialAlexCoder/killbricklua/refs/heads/main/src/scr.lua", true()))()
